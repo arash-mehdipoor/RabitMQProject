f1b99a43615413d0292eb05e2b08bcd7320775da
Arash Mehdipour <arash201221@gmail.com>
2022-01-02 21:51:34 +0330

    Add project files.

---
+++ RabitMQProject.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabitMQProject.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(queue: "InboxQueue", autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ RabitMQProject.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabitMQProject.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "InboxQueue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+            var message = "";
+            message = Console.ReadLine();
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange:"",
+                routingKey: "InboxQueue",
+                body: body
+                );
+            Console.WriteLine($"Message sent {message}");
+        }
+    }
+}
